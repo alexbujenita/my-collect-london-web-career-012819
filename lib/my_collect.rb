e19bb44fc2d2d7dc7e0516be44b5aa76c68478bc
Alex Bujenita <dekc89@gmail.com>
2018-12-09 18:56:24 +0000

    map simulator

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect array
+  if block_given?
+
+    i = 0
+    map_simulator = []
+
+    while i < array.length
+      map_simulator.push(yield(array[i]))
+      i+=1
+    end
+
+    map_simulator
+  else
+    print "Gimme a block!"
+  end
+
+end
 
 
+### TEST ###
+# my_collect([1,2,3,4,5,"Six"]) do |e|
+#   e
+# end
